49c8a3d1572783951387e4458a75ff5609c1e8fb
Jonathan Colby <jonathan.colby@unbelievable-machine.com>
2016-02-05 11:36:23 +0100

    detect vagrant local install

--- create-docker-registry-ovf.sh
+++ create-docker-registry-ovf.sh
@@ -6,20 +6,26 @@
 #######################################
 # user defined variables
 #######################################
+
+# output directory for virtualbox ovf
 OUTPUT_DIRECTORY="docker-registry-virtualbox"
+# packer json template
 PACKER_JSON="docker-registry.json"
 DOMAIN="n24.de"
+# admin user name
 USERNAME="weltn24"
+# admin user password
 PASSWORD="weltn24"
+# vm label
 VM_NAME="weltn24-docker-registry"
+# vm hostname
 HOSTNAME="weltn24-docker-registry"
 HTTP_DIR=./http
-PRESEED_FILE=$HTTP_DIR/preseed.cfg
-PRESEED_TEMPLATE=$HTTP_DIR/preseed.cfg.template
+# import and start the virtualbox image automatically?
 START_VIRTUALBOX_LOCALLY=true
+# run the registry in insecure mode?
 INSECURE_REGISTRY=true
-
-# Static IP for VirtualBox host-only networking on eth1 (eth0 is dhcp, NAT)
+# Static IP for VirtualBox interface eth1 (eth0 is dhcp, NAT)
 IP_ADDRESS=192.168.56.101
 
 # comment out to run packer in quite mode
@@ -29,13 +35,21 @@ PACKER_LOG=YES
 # build script
 #######################################
 
+PRESEED_FILE=$HTTP_DIR/preseed.cfg
+PRESEED_TEMPLATE=$HTTP_DIR/preseed.cfg.template
+
 # setup provisioning script creates eth1 for host-only or bridged networking in VirtualBox
 NETWORK="${IP_ADDRESS%.*}.0"
 BROADCAST="${IP_ADDRESS%.*}.255"
-sed -e "s/__IP_ADDRESS__/$IP_ADDRESS/" -e "s/__NETWORK__/$NETWORK/" -e "s/__BROADCAST__/$BROADCAST/" -e "s/__USERNAME__/$USERNAME/" scripts/setup.sh.template >scripts/setup.sh
+GATEWAY="${IP_ADDRESS%.*}.1"
+
+sed -e "s/__IP_ADDRESS__/$IP_ADDRESS/" -e "s/__GATEWAY__/$GATEWAY/" -e "s/__NETWORK__/$NETWORK/" -e "s/__BROADCAST__/$BROADCAST/" -e "s/__USERNAME__/$USERNAME/" scripts/setup.sh.template >scripts/setup.sh
 
 hash VBoxManage
 [[ $? -eq 0 ]] && HAS_VBOX=true
+
+hash vagrant
+[[ $? -eq 0 ]] && HAS_VAGRANT=true
 
 if [[ "$HAS_VBOX" = true ]] && [[ "$START_VIRTUALBOX_LOCALLY" = true ]] ;then
     echo "VirtualBox is installed locally. The generated OVF will be imported after creation"
@@ -60,7 +74,7 @@ eval "$(cat $PRESEED_TEMPLATE| sed 's/"/+++/g'|sed 's/^\(.*\)$/echo "\1"/')" |se
 
 if [[ "$INSECURE_REGISTRY" = true ]] ; then
   # create DOCKER_OPTS for insecure registry
-  echo 'sudo echo DOCKER_OPTS="--insecure-registry myregistrydomain.com:5000" >>/etc/default/docker' >>scripts/setup.sh
+  echo 'sudo echo DOCKER_OPTS="--insecure-registry myregistrydomain.com:5000" >>/etc/default/docker' >>scripts/cleanup.sh
 fi
 
 sed -e "s/__INSECURE_REGISTRY__/$INSECURE_REGISTRY/" scripts/docker_registry_install.sh.template > scripts/docker_registry_install.sh
@@ -96,6 +110,7 @@ if [[ $VBOXINSTALLED = true ]] ; then
  VBoxManage startvm "$VM_NAME" --type headless
 fi
 
-vagrant box remove $VM_NAME -f
-vagrant box add --name $VM_NAME vagrant/${VM_NAME}_virtualbox.box
-
+if [[ "$HAS_VAGRANT" = true ]] ; then
+  vagrant box remove $VM_NAME -f
+  vagrant box add --name $VM_NAME vagrant/${VM_NAME}_virtualbox.box
+fi
